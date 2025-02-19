49d60e58d2f14145a5b0321e029a2f929fabbf82
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-19 14:13:39 -0400

    Added cors header to lamda

--- backend/product_service/lambda/getProductsList.ts
+++ backend/product_service/lambda/getProductsList.ts
@@ -1,13 +1,17 @@
 export const handler = async (event: any) => {
     const products = [
-        { id: "1", name: "Product A", price: 100 },
-        { id: "2", name: "Product B", price: 200 },
-        { id: "3", name: "Product C", price: 300 },
+        {id: "1", name: "Product A", price: 100},
+        {id: "2", name: "Product B", price: 200},
+        {id: "3", name: "Product C", price: 300},
     ];
 
     return {
         statusCode: 200,
-        headers: { "Content-Type": "application/json" },
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "https://www.example.com",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         body: JSON.stringify(products),
     };
 };
