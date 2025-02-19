60f3dba2cb0d9783f8d41e19e56eca29c0f29acc
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-19 10:45:38 -0400

    Committed backend changes for Task 3.1

--- backend/product_service/lib/product_service-stack.ts
+++ backend/product_service/lib/product_service-stack.ts
@@ -1,16 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const getProductsListLambda = new lambda.Function(this, 'GetProductsListLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'lambda/getProductsList.handler',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ProductServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const api = new apigateway.RestApi(this, 'ProductServiceApi', {
+      restApiName: 'Product Service',
+    });
+
+    const products = api.root.addResource('products');
+
+    products.addMethod('GET', new apigateway.LambdaIntegration(getProductsListLambda));
   }
 }
