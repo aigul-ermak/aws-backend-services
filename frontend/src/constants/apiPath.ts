e02956c4740409cfbc46ed99e4d096bc779e700e
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-24 21:55:46 -0400

    Changed API path

--- frontend/src/constants/apiPath.ts
+++ frontend/src/constants/apiPath.ts
@@ -1,1 +1,1 @@
-export const API_URL = "https://3ooyz8ntq1.execute-api.us-east-1.amazonaws.com/prod/products/";
+export const API_URL = "https://hpwin4icp8.execute-api.us-east-1.amazonaws.com/dev/products";
