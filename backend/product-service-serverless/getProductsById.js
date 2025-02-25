d0c73efb5b4dda51ce4a75bd3ec7ec5ef8219e7c
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-25 16:49:18 -0400

    Added lamda functions for backend

---
+++ backend/product-service-serverless/getProductsById.js
@@ -0,0 +1,58 @@
+module.exports.handler = async (event) => {
+    if (event.httpMethod === "OPTIONS") {
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, OPTIONS"
+            },
+            body: ""
+        };
+    }
+
+    const productId = event.pathParameters?.productId;
+    if (!productId) {
+        return {
+            statusCode: 400,
+            headers: {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, OPTIONS"
+            },
+            body: JSON.stringify({ message: "Missing productId" }),
+        };
+    }
+
+    const products = [
+        { id: "1", name: "Product A", price: 100 },
+        { id: "2", name: "Product B", price: 200 },
+        { id: "3", name: "Project C", price: 300 },
+    ];
+
+    const product = products.find(p => p.id === productId);
+
+    if (!product) {
+        return {
+            statusCode: 404,
+            headers: {
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, OPTIONS"
+            },
+            body: JSON.stringify({ message: "Product not found" }),
+        };
+    }
+
+
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, OPTIONS"
+        },
+        body: JSON.stringify(product),
+    };
+};
