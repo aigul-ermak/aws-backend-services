35a8c1a1e54695c58427e7ce795a5eb4e1a81062
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-03-07 15:57:15 -0400

    Added butons for upload file

--- frontend/src/components/pages/PageProducts/PageProducts.tsx
+++ frontend/src/components/pages/PageProducts/PageProducts.tsx
@@ -1,9 +1,18 @@
 import Products from "~/components/pages/PageProducts/components/Products";
 import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
+import UploadCSV from "~/components/UploadCSV";
 
 export default function PageProducts() {
+  const importApiUrl =
+    "https://obyvp4e5m7.execute-api.us-east-1.amazonaws.com/prod/import";
+
   return (
     <Box py={3}>
+      <Typography variant="h4" gutterBottom>
+        Welcome to My Store
+      </Typography>
+      <UploadCSV importApiUrl={importApiUrl} />
       <Products />
     </Box>
   );
