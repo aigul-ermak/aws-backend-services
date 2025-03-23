4188f7b62db7b8f140d1059beec37101aaacfa82
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-03-23 17:29:25 -0300

    feat(auth): updated basicAutjorized

--- backend/my-backend-api/authorization-service/src/handlers/basicAuthorizer.ts
+++ backend/my-backend-api/authorization-service/src/handlers/basicAuthorizer.ts
@@ -1,18 +1,49 @@
-import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
+import { APIGatewayTokenAuthorizerEvent, APIGatewayAuthorizerResult, StatementEffect } from 'aws-lambda';
+import * as dotenv from 'dotenv';
 
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    try {
+dotenv.config();
 
-    } catch (error) {
-        console.error("Error generating signed URL:", error);
-        return {
-            statusCode: 500,
-            headers: {
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET, OPTIONS"
-            },
-            body: JSON.stringify({message: "Internal Server Error"}),
-        };
+export const basicAuthorizer = async (
+    event: APIGatewayTokenAuthorizerEvent
+): Promise<APIGatewayAuthorizerResult> => {
+    console.log('Received event:', event);
+
+    if (!event.authorizationToken) {
+        throw new Error('Unauthorized'); // API Gateway returns 401 automatically
     }
-}
+
+    try {
+        const encodedCredentials = event.authorizationToken.split(' ')[1];
+        const buffer = Buffer.from(encodedCredentials, 'base64');
+        const [username, password] = buffer.toString('utf-8').split(':');
+
+        const storedPassword = process.env[username];
+
+        if (!storedPassword || storedPassword !== password) {
+            throw new Error('Forbidden');
+        }
+
+        return generatePolicy(username, event.methodArn, 'Allow');
+    } catch (error) {
+        console.error('Error:', error);
+        throw new Error('Forbidden');
+    }
+};
+
+const generatePolicy = (
+    principalId: string,
+    resource: string,
+    effect: StatementEffect
+): APIGatewayAuthorizerResult => ({
+    principalId,
+    policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+            {
+                Action: 'execute-api:Invoke',
+                Effect: effect,
+                Resource: resource,
+            },
+        ],
+    },
+});
